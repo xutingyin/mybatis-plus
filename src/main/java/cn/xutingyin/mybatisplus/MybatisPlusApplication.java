488c41ed82daf2e434955ba580200cae7723b0ef
sunshinexuty <sunshinexuty@163.com>
2019-10-18 16:09:44 +0800

    增加注释

--- src/main/java/cn/xutingyin/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/cn/xutingyin/mybatisplus/MybatisPlusApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+/** 
+* @Description: mybatisPlus 启动类
+* @Author: xuty 
+* @Date: 2019/10/18 16:06
+*/
 
 @SpringBootApplication
 @MapperScan("cn.xutingyin.mybatisplus.dao")
