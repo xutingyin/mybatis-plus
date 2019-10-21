ff74087a752c24f473982e9f3a1d318aff64d0e6
sunshinexuty <sunshinexuty@163.com>
2019-10-21 15:10:30 +0800

    修改视图层实体类

--- src/main/java/cn/xutingyin/mybatisplus/vo/PaperVo.java
+++ src/main/java/cn/xutingyin/mybatisplus/vo/PaperVo.java
@@ -1,13 +1,22 @@
 package cn.xutingyin.mybatisplus.vo;
 
+import cn.xutingyin.mybatisplus.dto.PaperDto;
 import cn.xutingyin.mybatisplus.entity.Paper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 
+import java.util.List;
+
+/** 
+* @Description: 书籍 视图层实体类 
+* @Author: xuty 
+* @Date: 2019/10/21 15:10
+*/
 @Data
 public class PaperVo {
-    private Integer code;
-    private String msg;
-    private Page<Paper> obj;
+    private Long pageSize;
+    private Long pageNum;
+    private Long total;
+    public List<PaperDto> list;
 
 }
