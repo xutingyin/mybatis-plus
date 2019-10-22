d57468090dec1cb02995307ad32a6cb83c10d7ac
sunshinexuty <sunshinexuty@163.com>
2019-10-22 11:29:16 +0800

    RabbitMQ基本使用

---
+++ src/main/java/cn/xutingyin/mybatisplus/config/FanoutRabbitConfig.java
@@ -0,0 +1,51 @@
+package cn.xutingyin.mybatisplus.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+* @Description: RabbitMQ 配置类
+* @Author: xuty 
+* @Date: 2019/10/22 9:28
+*/
+@Configuration
+public class FanoutRabbitConfig {
+    @Bean
+    public Queue aMessage() {
+        return new Queue("fanout.A");
+    }
+
+    @Bean
+    public Queue bMessage() {
+        return new Queue("fanout.B");
+    }
+
+    @Bean
+    public Queue cMessage() {
+        return new Queue("fanout.C");
+    }
+
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange("fanoutExchange");
+    }
+
+    @Bean
+    Binding bindingExchangeA(Queue aMessage, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(aMessage).to(fanoutExchange);
+    }
+
+    @Bean
+    Binding bindingExchangeB(Queue bMessage, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(bMessage).to(fanoutExchange);
+    }
+
+    @Bean
+    Binding bindingExchangeC(Queue cMessage, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(cMessage).to(fanoutExchange);
+    }
+}
---
+++ src/main/java/cn/xutingyin/mybatisplus/config/TopicRabbitConfig.java
@@ -0,0 +1,60 @@
+package cn.xutingyin.mybatisplus.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/** 
+* @Description: RabbitMQ 配置类
+* @Author: xuty 
+* @Date: 2019/10/22 9:28
+*/
+@Configuration
+public class TopicRabbitConfig {
+
+
+    final static String message = "topic.message";
+    final static String messages = "topic.messages";
+
+    @Bean
+    public Queue queueMessage() {
+        return new Queue(TopicRabbitConfig.message);
+    }
+
+    @Bean
+    public Queue queueMessages() {
+        return new Queue(TopicRabbitConfig.messages);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("exchange");
+    }
+
+    @Bean
+    Binding bindingExchangeMessage(Queue queueMessage, TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessage).to(exchange).with("topic.message");
+    }
+
+    @Bean
+    Binding bindingExchangeMessages(Queue queueMessages, TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessages).to(exchange).with("topic.#");
+    }
+    
+    /*@Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+    @Bean(name = "rabbitListenerContainerFactory")
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }*/
+}
